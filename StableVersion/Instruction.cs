9edf1f647cc0e8b36841c28fc20bc72f7bb2e0e5
GUERRAICHE AHMED AMINE <la_guerraiche@esi.dz>
2023-04-27 17:33:52 +0100

    I changed mainly in Jeux Instruction -- valeur par default pour les arguments et ajout de type_execution(soit memoire ou programme) + modification dans la classe instruction

--- StableVersion/Instruction.cs
+++ StableVersion/Instruction.cs
@@ -8,24 +8,72 @@ namespace ArchiMind
         private string format;
         private string cop;
         private string champ_notused;
+        private string mnemonique;
+        private bool mem; //mem ou reg
+        private string destination;
+        private string source;
+        private bool ifdepl;
+        private string valdep;
+
         public void setFormat (string format) { this.format = format; }
         public void setCop (string cop) { this.cop= cop; }
         public string getFormat () { return this.format; }
         public string getCop() { return this.cop; }
-         public Instruction ()
+        public string getMnemonique()
         {
+               return this.mnemonique;
+        }
+        public bool getRegM()
+        {
+            return mem; 
+        }
+        public string getSource()
+        {
+            return this.source;
+        }
+        public string getDestination()
+        {
+            return this.destination;
+        }
+        public bool getifdepl()
+        {
+            return this.ifdepl;
+        }
+        public string getValDepl()
+        {
+            return this.valdep;
+        }
+        public void setIfDepl(bool ifdepl)
+        {
+            this.ifdepl = ifdepl;
+        }
+        public void setMem(bool mem)
+        {
+            this.mem = mem;
+        }
+
+        public Instruction (){
         }  
-        public Instruction (string format , string cop )
-        {
+        public Instruction (string format , string cop ){
             this.format = format;
             this.cop = cop;
         }  
-        public Instruction (string format , string cop ,string champ_notused)
-        {
+        
+        public Instruction (string format , string cop ,string champ_notused){
             this.format = format;
             this.cop = cop;
             this.champ_notused=champ_notused;
         }  
+        public Instruction(string format, string cop, string champ_notused, string mnemonique, bool mem, string destination, string source, bool ifdepl, string valdep) : this(format, cop, champ_notused){
+            this.mnemonique = mnemonique;
+            this.mem = mem;
+            this.destination = destination;
+            this.source = source;
+            this.ifdepl = ifdepl;
+            this.valdep = valdep;
+        }
+
+
         public void afficheInstruction()
         {
             Console.Write(getFormat()+" ");
