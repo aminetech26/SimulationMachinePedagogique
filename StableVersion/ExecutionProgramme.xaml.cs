c46f5b21486da4987c73495210b3f30d91df6732
ilyesblidi <ll_blidi@esi.dz>
2023-05-01 00:12:25 +0100

    Addition du Mémoire au page ExécutionProgramme

--- StableVersion/ExecutionProgramme.xaml.cs
+++ StableVersion/ExecutionProgramme.xaml.cs
@@ -32,11 +32,65 @@ namespace projet
             DI.Text = Registre.getDi() ;
             SP.Text = Registre.getSp() ;
             BP.Text = Registre.getBp() ;
+
+            List<Case> _animals;
+
+            _animals = new List<Case>();
+            _animals.Add(new Case { ADR = "", Donnee = "" });
+            _animals.Add(new Case { ADR = "100h", Donnee = "8197h" });
+            _animals.Add(new Case { ADR = "101h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "102h", Donnee = "8197h" });
+            _animals.Add(new Case { ADR = "103h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "104h", Donnee = "8165h" });
+            _animals.Add(new Case { ADR = "105h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "106h", Donnee = "9765h" });
+            _animals.Add(new Case { ADR = "107h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "108h", Donnee = "9765h" });
+            _animals.Add(new Case { ADR = "109h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "110h", Donnee = "8765h" });
+            _animals.Add(new Case { ADR = "111h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "112h", Donnee = "8765h" });
+            _animals.Add(new Case { ADR = "113h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "100h", Donnee = "8165h" });
+            _animals.Add(new Case { ADR = "101h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "100h", Donnee = "8195h" });
+            _animals.Add(new Case { ADR = "101h", Donnee = "0187h" });
+            _animals.Add(new Case { ADR = "100h", Donnee = "8195h" });
+            _animals.Add(new Case { ADR = "101h", Donnee = "0187h" });
+            dataGrid1.ItemsSource = _animals;
+
         }
 
         private void Button_Go_Back_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.GoBack();
+        }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dataGrid1.SelectedItem != null)
+            {
+                // Get the selected row object
+                Case? selectedPerson = dataGrid1.SelectedItem as Case;
+
+                // Display the selected person's name in a TextBox
+                //txADR.Text = selectedPerson.ADR;
+                RIM.Text = selectedPerson.Donnee;
+            }
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+            {
+                if (e.NewSize.Width >= 1000)
+                {
+                    //dataGrid1.Width = 350 ;
+                }
+                else
+                {
+                    //dataGrid1.Width = 250;
+
+                }
+
         }
     }
 }
